efd6cf07602f467f428b4d2e72da7b38e6e4430d
mizu4my <mizu4my@gmail.com>
2022-02-12 18:06:14 +0900

    created Post model

--- coordinate_yohou/settings.py
+++ coordinate_yohou/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'coordinate_yohou.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'coordinate_yohou',
+        'USER': 'postgres',
+        'PASSWORD': 'Vio#lin01',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- yohou/models.py
+++ yohou/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+CATEGORY_CHOICES = (
+    (1, 'WOMEN'),
+    (2, 'MEN'),
+    (3, 'KIDS'),
+)
+
+WEATHER_CHOICES = (
+    (1, '晴れ'),
+    (2, '曇り'),
+    (3, '雨')
+)
+
+
+class Post(models.Model):
+    author = models.CharField(max_length=30, blank=False, null=False)
+    category = models.IntegerField(choices=CATEGORY_CHOICES)
+    temperature = models.IntegerField(blank=False, null=False)
+    weather = models.IntegerField(choices=WEATHER_CHOICES)
+    title = models.CharField(max_length=30)
+    text = models.TextField(blank=True, null=True)
+    brands_tops = models.CharField(max_length=30, blank=True, null=True)
+    brands_bottoms = models.CharField(max_length=30, blank=True, null=True)
+    brands_shoes = models.CharField(max_length=30,blank=True, null=True)
+    brands_outer = models.CharField(max_length=30, blank=True, null=True)
+    brands_accesory = models.CharField(max_length=30, blank=True, null=True)
+    photo = models.FileField(upload_to = 'static/media/posts', blank=False, null=False)
