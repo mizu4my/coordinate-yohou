4ca6c3bc9700cc4b22785baf1ffa02c6416ab9a4
mizu4my <mizu4my@gmail.com>
2022-02-13 16:51:21 +0900

    edited views

--- yohou/urls.py
+++ yohou/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.top, name='top'),
     path('search_results', views.search_results, name="search_results"),
 ]
